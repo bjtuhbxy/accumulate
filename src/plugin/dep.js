7f8a676e2fec52223c471cd389f40d694415bd07
zhangw <zhangw@bokecc.com>
2021-05-31 20:38:05 +0800

    +store v1

---
+++ src/plugin/dep.js
@@ -0,0 +1,59 @@
+class Dep {
+  constructor () {
+    this.dep = {}
+  }
+
+  /**
+   * @description 添加订阅者
+   * @param {*} key 需要订阅的字段名称
+   * @param {*} fn 订阅方法的回调函数
+   * @returns 订阅者的唯一id
+   */
+  sub (key, fn) {
+    this.dep[key] = this.dep[key] || {}
+    let id = Symbol('11')
+    this.dep[key][id] = fn
+    return id
+  }
+
+  /**
+   * @description 向订阅者发布订阅变更
+   * @param {*} key 订阅的发生改变的字段
+   * @returns {null}
+   */
+  pub (key) {
+    let args = Array.from(arguments)
+    let pubList = this.dep[key]
+    let pubListSymbol = Object.getOwnPropertySymbols(pubList)
+    if (!pubList) {
+      // 变量未被订阅，不作任何处理
+      return
+    }
+    args.shift()
+    for (const k in pubListSymbol) {
+      if (Object.hasOwnProperty.call(pubListSymbol, k)) {
+        const fn = pubList[pubListSymbol[k]];
+        fn(...args)
+      }
+    }
+  }
+  
+  /**
+   * @description 删除订阅关系
+   * @param {*} key 订阅的变量关键字
+   * @param {*} id 添加订阅者方法返回的唯一id
+   */
+  delete (key, id) {
+    delete this.dep[key][id]
+  }
+
+  /**
+   * @description 获取唯一随机id
+   * @param {*} range 唯一id随机数区间
+   * @returns {String} 唯一id
+   */
+  static getId (range = 1000) {
+    return 'T' + new Date().getTime() + 'R' + parseInt(Math.random() * range)
+  }
+}
+export default Dep
---
+++ src/plugin/store.js
@@ -0,0 +1,73 @@
+/**
+ * @description 状态管理插件
+ * @author zhangw
+ * @date 2021-05-23
+ * @version 1.0.0
+ */
+import Dep from "./dep.js";
+class Store {
+  constructor(option) {
+    this.state = option.state
+    this.mutations = option.mutations
+    this.write = false
+    Store.handleProperty.call(this)
+    this.dep = new Dep()
+    this.sub = this.dep.sub.bind(this.dep)
+    this.delete = this.dep.delete.bind(this.dep)
+  }
+  /**
+   * @description 严格管理数据，防止直接修改（只允许mutations修改）
+   * @param {*} state 需要setter处理的数据源
+   */
+  static handleProperty() {
+    let state = this.state
+    Object.keys(state).forEach(item => {
+      let key = state[item]
+      Object.defineProperty(state, item, {
+        get: () => {
+          return key
+        },
+        set: (val) => {
+          if (this.write) {
+            key = val
+            this.dep.pub(item, val)
+          }
+        }
+      })
+    })
+  }
+  commit(key) {
+    let args = Array.from(arguments)
+    args.shift()
+    this.write = true
+    this.mutations[key](this.state, ...args)
+    this.write = false
+  }
+}
+/**
+ * @description 单元测试
+ */
+window.store = new Store({
+  state: {
+    online_users: [],
+    user_setting: []
+  },
+  mutations: {
+    changeOnlineUser(state, val) {
+      state.online_users = val
+    },
+    changeUserSetting(state, val) {
+      state.user_setting = val
+    }
+  }
+})
+// 测试订阅数据
+let sub1 = store.sub('online_users', item => {
+  console.log('【cc log】监听到人员列表变化:新值为' + item);
+})
+// 删除订阅者
+// store.delete('online_users', sub1)
+// 测试更新数据
+console.log(store.state.online_users);
+store.commit('changeOnlineUser', ['李四', '王五'])
+console.log(store.state.online_users);
